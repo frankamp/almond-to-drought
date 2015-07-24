16ed53699616e851676a1c7e5168c7062347e495
frankamp <frankamp@gmail.com>
2015-07-24 10:34:46 -0700

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bAlmond\b/g, "Drought");
+	v = v.replace(/\balmond\b/g, "drought");
+	v = v.replace(/\bAlmonds\b/g, "Drought Causers");
+	v = v.replace(/\balmonds\b/g, "drought causers");
 	
 	textNode.nodeValue = v;
 }
